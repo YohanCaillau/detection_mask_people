e164fa53f118efc2e07effbf8fee84b6530734ad
Yohan Caillau <yohancaillau@live.fr>
2020-10-08 11:09:50 +0200

    fonctionne sur webcam

--- 1.0.0/utils/image_utils/image_saver.py
+++ 1.0.0/utils/image_utils/image_saver.py
@@ -1,10 +1,10 @@
 import cv2
 import os
 
-vehicle_count = [0]
+person_count = [0]
 
 current_path = os.getcwd()
 
 def save_image(source_image):
-	cv2.imwrite(current_path + "/detected_objects/object" + str(len(vehicle_count)) + ".png", source_image)
-	vehicle_count.insert(0,1)
+	cv2.imwrite(current_path + "/detected_objects/object" + str(len(person_count)) + ".png", source_image)
+	person_count.insert(0,1)
--- 1.0.0/utils/object_counting_module/object_counter.py
+++ 1.0.0/utils/object_counting_module/object_counter.py
@@ -1,7 +1,7 @@
 from utils.image_utils import image_saver
 
-is_vehicle_detected = [0]
-bottom_position_of_detected_vehicle = [0]
+is_person_detected = [0]
+bottom_position_of_detected_person = [0]
 
 def count_objects(top, bottom, right, left, crop_img, roi_position, y_min, y_max, deviation):   
         direction = "n.a." # means not available, it is just initialization
@@ -9,16 +9,16 @@ def count_objects(top, bottom, right, left, crop_img, roi_position, y_min, y_max
         update_csv = False
 
         if (abs(((bottom+top)/2)-roi_position) < deviation):
-          is_vehicle_detected.insert(0,1)
+          is_person_detected.insert(0,1)
           update_csv = True
           image_saver.save_image(crop_img) # save detected object image
 
-        if(bottom > bottom_position_of_detected_vehicle[0]):
-                direction = "down"
+        if(bottom > bottom_position_of_detected_person[0]):
+                direction = "Enter"
         else:
-                direction = "up"
+                direction = "Exit"
 
-        bottom_position_of_detected_vehicle.insert(0,(bottom))
+        bottom_position_of_detected_person.insert(0,(bottom))
 
-        return direction, is_vehicle_detected, update_csv
+        return direction, is_person_detected, update_csv
 
